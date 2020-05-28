4b3dfe4bf2aceece8d8d4b638610404052f62e63
Jannik Junker <geisterhauserde@gmail.com>
2020-05-28 15:26:24 +0200

    A few fixes and added a Database

--- src/main/java/org/dhbw/classes/Databank.java
+++ src/main/java/org/dhbw/classes/Databank.java
@@ -6,23 +6,20 @@ import java.util.Calendar;
 import java.util.List;
 
 public class Databank {
-    private static final String databankPath = "";
-    private static Driver driver = null;
 
-    public static Object[] GetFromDatabank(String command, Class get_c, int column) {
+    public static Object[] GetFromDatabank(String command, int column) {
         try {
-            DriverManager.registerDriver(driver);
-            Connection con = DriverManager.getConnection(databankPath);
+            Connection con = getConnection();
             Statement statement = con.createStatement();
             ResultSet resultSet = statement.executeQuery(command);
             List o = new ArrayList();
             while (resultSet.next())
-                o.add(resultSet.getObject(column, get_c));
+                o.add(resultSet.getObject(column));
             resultSet.close();
             statement.close();
             con.close();
             return o.toArray();
-        } catch (SQLException throwable) {
+        } catch (SQLException | ClassNotFoundException throwable) {
             throwable.printStackTrace();
         }
         return null;
@@ -33,16 +30,21 @@ public class Databank {
         if (objects == null) objects = new Object[0];
         if (set == null) set = new SQLType[0];
         try {
-            DriverManager.registerDriver(driver);
-            Connection con = DriverManager.getConnection(databankPath);
+            Connection con = getConnection();
             PreparedStatement preparedStatement = con.prepareStatement(command);
             for (int f = 0; f < objects.length && f < set.length; f++)
                 preparedStatement.setObject(f + 1, objects[f], set[f]);
             preparedStatement.executeUpdate();
             preparedStatement.close();
             con.close();
-        } catch (SQLException throwable) {
+        } catch (SQLException | ClassNotFoundException throwable) {
             throwable.printStackTrace();
         }
+    }
+
+    public static Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        return DriverManager.getConnection(
+                "jdbc:mysql://85.214.247.101:3306/dhbw","mlg_dhbw","Reisebus1!");
     }
 }
--- src/main/java/org/dhbw/classes/Main.java
+++ src/main/java/org/dhbw/classes/Main.java
@@ -1,5 +1,6 @@
 package org.dhbw.classes;
 
+import java.sql.SQLType;
 import java.util.Date;
 
 public class Main {
@@ -7,5 +8,10 @@ public class Main {
         University DHBW=new University("Duale Hochschule Baden-Wüttenberg Mannheim");
         DHBW.addDocent(new Docent("Kruse", "Eckhard", new Date(2000,11,1),new Address("hh","34","2343","Hung","33")));
         System.out.println("TEST");
+        //Databank.SetToDatabank("INSERT INTO student (`student_id`, `matriculation_number`, `person_id`, `java_knowlage`, `course_id`, `company_id`) VALUES ('2', '213221', '1', '12', '1', '1');", null, new SQLType[0]);
+        for (Object o : Databank.GetFromDatabank("SELECT * FROM student", 1)) {
+            if(o instanceof Integer)
+                System.out.println((int) o);
+        }
     }
 }
