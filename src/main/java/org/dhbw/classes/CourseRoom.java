3471686c60f6532fbcce8631832fa9695281ed5d
Gnuhry <truongxh1@gmail.com>
2020-06-22 23:36:31 +0200

    Add more room info

--- src/main/java/org/dhbw/classes/CourseRoom.java
+++ src/main/java/org/dhbw/classes/CourseRoom.java
@@ -2,16 +2,64 @@ package org.dhbw.classes;
 
 import java.util.Objects;
 
-public class CourseRoom implements Comparable<CourseRoom>{
-    private final String name;
+public class CourseRoom implements Comparable<CourseRoom> {
+    private final String name, building, floor;
+    private int seatsAmount;
+    private boolean documentCamera, laboratory;
 
-    public CourseRoom(String name) {
+    public CourseRoom(String name, String building, String floor) {
         this.name = name;
+        this.building = building;
+        this.floor = floor;
+        this.seatsAmount = 0;
+        documentCamera = laboratory = false;
+    }
+
+    public CourseRoom(String name, String building, String floor, int seatsAmount, boolean documentCamera, boolean laboratory) {
+        this.name = name;
+        this.building = building;
+        this.floor = floor;
+        this.seatsAmount = seatsAmount;
+        this.documentCamera = documentCamera;
+        this.laboratory = laboratory;
     }
 
     //------------------Getter-----------------------
     public String getName() {
         return name;
+    }
+
+    public String getBuilding() {
+        return building;
+    }
+
+    public String getFloor() {
+        return floor;
+    }
+
+    public int getSeatsAmount() {
+        return seatsAmount;
+    }
+
+    public boolean hasDocumentCamera() {
+        return documentCamera;
+    }
+
+    public boolean isLaboratory() {
+        return laboratory;
+    }
+
+    //-----------------------Setter-----------------
+    public void setSeatsAmount(int seatsAmount) {
+        this.seatsAmount = seatsAmount;
+    }
+
+    public void setDocumentCamera(boolean documentCamera) {
+        this.documentCamera = documentCamera;
+    }
+
+    public void setLaboratory(boolean laboratory) {
+        this.laboratory = laboratory;
     }
 
     //------------------Overrides---------------------
